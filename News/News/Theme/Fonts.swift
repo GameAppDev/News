d7f1ad6fa4d201dde27ef1d2cfc62075bcfd5e26
OguzhanYalcin <oyalcin@innova.com.tr>
2022-10-07 18:19:53 +0300

    fonts
    Fonts are changed.

--- News/News/Theme/Fonts.swift
+++ News/News/Theme/Fonts.swift
@@ -10,8 +10,8 @@ import UIKit
 
 extension UIFont {
     
-    static let navbarTitleFont = UIFont(name: "SourceSansPro-Bold", size: CGFloat(32).ws)
-    static let buttonTitleFont = UIFont(name: "SourceSansPro-SemiBold", size: CGFloat(24).ws)
-    static let textFont        = UIFont(name: "SourceSansPro-Regular", size: CGFloat(16).ws)
-    static let titleFont       = UIFont(name: "SourceSansPro-SemiBold", size: CGFloat(16).ws)
+    static let navbarTitleFont = UIFont(name: "Nunito-Bold", size: CGFloat(26).ws)
+    static let buttonTitleFont = UIFont(name: "Nunito-Medium", size: CGFloat(24).ws)
+    static let textFont        = UIFont(name: "Nunito-Regular", size: CGFloat(16).ws)
+    static let titleFont       = UIFont(name: "Nunito-Medium", size: CGFloat(16).ws)
 }

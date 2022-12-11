207fb6a19e716121c290e70a26dc6abf4ebad647
OguzhanYalcin <oguzhan.yalcin@partner.turktelekom.com.tr>
2022-12-11 14:20:58 +0300

    Int+Ext is added.

---
+++ News/News/Helpers/Extensions/Int+Ext.swift
@@ -0,0 +1,15 @@
+//
+//  Int+Ext.swift
+//  News
+//
+//  Created by Oguzhan Yalcin on 11.12.2022.
+//
+
+import Foundation
+
+extension Int {
+    
+    func toString() -> String {
+        return "\(self)"
+    }
+}
